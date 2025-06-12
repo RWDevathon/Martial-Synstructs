6dd67f9ade6167f3b40dd50fe649bfc8e35de50b
Killathon <huelva21@gmail.com>
2025-06-12 19:03:26 -0400

    1.6 Experimental

---
+++ Source/v1.6/JuggernautUtilities/MoteMaker_JuggernautDescent.cs
@@ -0,0 +1,18 @@
+﻿using RimWorld;
+using Verse;
+
+namespace ArtificialBeings
+{
+    public static class MoteMaker_JuggernautDescent
+    {
+        public static void MakeMote_JuggernautDescent(IntVec3 cell, Map map)
+        {
+            Mote mote = (Mote)ThingMaker.MakeThing(ThingDefOf.Mote_Bombardment, null);
+            mote.exactPosition = cell.ToVector3Shifted();
+            mote.Scale = 5f;
+            mote.rotationRate = 0f;
+            GenSpawn.Spawn(mote, cell, map);
+        }
+
+    }
+}
